6c61626e23e3dbfe86cd99b62609470f4eca7f04
marco-vanin <marco.vanin.pro@gmail.com>
2024-01-25 13:34:14 +0100

    🚧 Start sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import ActorDetails from "./pages/ActorDetails";
-import Home from "./pages/Home";
 import MovieDetails from "./pages/MovieDetails";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
     <div className="flex min-h-screen">
-      {/* <Sidebar /> */}
+      <Sidebar />
       <div className="flex flex-col flex-grow">
         <Navbar />
         <div className="flex-grow p-6">
           <Routes>
-            <Route path="/" element={<Home />} />
+            {/* <Route path="/" element={<Home />} /> */}
             <Route path="/movie/:id" element={<MovieDetails />} />
             <Route path="/actor" element={<ActorDetails />} />
           </Routes>
---
+++ src/components/Sidebar/SidebarItem/type.ts
@@ -0,0 +1,10 @@
+import { LucideIcon } from "lucide-react";
+
+export type SideBarItemType = {
+  id: number;
+  label: string;
+  link: string;
+  icon: LucideIcon;
+};
+
+export default SideBarItemType;
